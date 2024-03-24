1ada800af4b2e7da5f8366590674e7fdd0ddf835
kgw <kimgw1386@gmail.com>
2024-03-24 16:59:02 +0900

    first commit

---
+++ src/main/java/solo/Oldclothescollectionbox/OldClothesCollectionBoxApplication.java
@@ -0,0 +1,13 @@
+package solo.Oldclothescollectionbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldClothesCollectionBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldClothesCollectionBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/solo/Oldclothescollectionbox/OldClothesCollectionBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package solo.Oldclothescollectionbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldClothesCollectionBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
